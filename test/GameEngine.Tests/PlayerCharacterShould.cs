e6680bdfc7bd008bbb490fc2c2e9d088ba251fb2
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-03-28 13:46:50 +0100

    Test assert Equal string

--- test/GameEngine.Tests/PlayerCharacterShould.cs
+++ test/GameEngine.Tests/PlayerCharacterShould.cs
@@ -13,7 +13,20 @@ namespace GameEngine.Tests
             bool isNoob = sut.isNoob;
             //Assert
             Assert.True(isNoob);
+        }
 
+        [Fact]
+        public void CalculateFullName()
+        {
+            //Arrange
+            PlayerCharacter sut = new PlayerCharacter();
+            sut.FirstName = "Kalle";
+            sut.LastName = "Anka";
+            string expected = "Kalle Anka";
+            //Act
+            string actual = sut.FullName;
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
